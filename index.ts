68d65753ac36914fa199549c7df913ebd86bbb2b
sodapng <sodapng@ya.ru>
2022-06-21 17:47:43 +0400

    refactor: draw line

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { httpServer } from './src/http_server/index'
 import robot from 'robotjs'
 import { createWebSocketStream, WebSocketServer } from 'ws'
 import printScreen from './src/helpers/printScreen'
+import drawLine from './src/helpers/drawLine'
 
 const HTTP_PORT = 3000
 
@@ -9,15 +10,6 @@ console.log(`Start static http server on the ${HTTP_PORT} port!`)
 httpServer.listen(HTTP_PORT)
 
 const wss = new WebSocketServer({ port: 8080 })
-
-const drawLine = (x: number, y: number, width: number, height?: number) => {
-  robot.mouseToggle('down')
-  robot.dragMouse(x + width, y)
-  robot.dragMouse(x + width, y + (height || width))
-  robot.dragMouse(x, y + (height || width))
-  robot.dragMouse(x, y)
-  robot.mouseToggle('up')
-}
 
 const drawCircle = (x: number, y: number, radius: number) => {
   robot.moveMouse(x + radius, y)
