9ce581c232932e4a728424e3053205134b404e82
sodapng <sodapng@ya.ru>
2022-06-21 17:53:14 +0400

    refactor: print screen

--- src/helpers/printScreen.ts
+++ src/helpers/printScreen.ts
@@ -8,8 +8,8 @@ export default async function printScreen(
   height: number
 ): Promise<Jimp> {
   return new Promise((resolve) => {
-    const capture = screen.capture(x, y, width, height)
-    const image = new Jimp(capture.width, capture.height)
+    const bitmap = screen.capture(x, y, width, height)
+    const image = new Jimp(bitmap.width, bitmap.height)
 
     let pos = 0
 
@@ -19,10 +19,10 @@ export default async function printScreen(
       image.bitmap.width,
       image.bitmap.height,
       (x, y, idx) => {
-        image.bitmap.data[idx + 2] = capture.image.readUInt8(pos++)
-        image.bitmap.data[idx + 1] = capture.image.readUInt8(pos++)
-        image.bitmap.data[idx + 0] = capture.image.readUInt8(pos++)
-        image.bitmap.data[idx + 3] = capture.image.readUInt8(pos++)
+        image.bitmap.data[idx + 2] = bitmap.image.readUInt8(pos++)
+        image.bitmap.data[idx + 1] = bitmap.image.readUInt8(pos++)
+        image.bitmap.data[idx + 0] = bitmap.image.readUInt8(pos++)
+        image.bitmap.data[idx + 3] = bitmap.image.readUInt8(pos++)
       }
     )
 
