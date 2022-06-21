fbc1ce769e91940c5a781452763a9f304eedc6f9
sodapng <sodapng@ya.ru>
2022-06-21 20:34:53 +0400

    fix: draw circle

--- src/helpers/drawCircle.ts
+++ src/helpers/drawCircle.ts
@@ -1,15 +1,15 @@
 import { dragMouse, mouseToggle, moveMouse } from 'robotjs'
 
 export default function drawCircle(x: number, y: number, radius: number) {
-  moveMouse(x + radius, y)
+  moveMouse(x - radius, y)
   mouseToggle('down')
 
   for (let i = 0; i <= Math.PI * 2; i += 0.01) {
-    const _x = x + radius * Math.cos(i)
-    const _y = y + radius * Math.sin(i)
+    const _x = x - radius * Math.cos(i)
+    const _y = y - radius * Math.sin(i)
 
     dragMouse(_x, _y)
-
-    mouseToggle('up')
   }
+
+  mouseToggle('up')
 }
