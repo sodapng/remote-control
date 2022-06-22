35d0573f2c5a4c746e6d2c456af77446e9706c1f
sodapng <sodapng@ya.ru>
2022-06-23 00:57:45 +0400

    feat: use stream for http server

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,18 +1,23 @@
-import { readFile } from 'fs'
+import { createReadStream } from 'fs'
 import { createServer } from 'http'
 import { dirname, resolve } from 'path'
+import { pipeline } from 'stream/promises'
 
-export const httpServer = createServer(function (req, res) {
-  const __dirname = resolve(dirname(''))
-  const file_path =
-    __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url)
-  readFile(file_path, function (err, data) {
-    if (err) {
-      res.writeHead(404)
-      res.end(JSON.stringify(err))
-      return
-    }
-    res.writeHead(200)
-    res.end(data)
-  })
+export const httpServer = createServer(async (req, res) => {
+  try {
+    const __dirname = resolve(dirname(''))
+    const file_path =
+      __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url)
+
+    const rs = createReadStream(file_path)
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8',
+    })
+
+    await pipeline(rs, res)
+  } catch (error) {
+    res.writeHead(404, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(error))
+  }
 })
