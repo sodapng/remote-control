ca1933ce167064da33cb4e90b302b53ced40f03a
sodapng <sodapng@ya.ru>
2022-06-23 23:20:59 +0400

    feat: fast draw circle

--- src/helpers/drawCircle.ts
+++ src/helpers/drawCircle.ts
@@ -4,7 +4,7 @@ export default function drawCircle(x: number, y: number, radius: number) {
   moveMouse(x - radius, y)
   mouseToggle('down')
 
-  for (let i = 0; i <= Math.PI * 2; i += 0.01) {
+  for (let i = 0; i <= Math.PI * 2; i += 0.01 * Math.PI) {
     const _x = x - radius * Math.cos(i)
     const _y = y - radius * Math.sin(i)
 
